158e0cf5e52833becb83c8698dc8cda35e7d15ef
Cole Ouyang <coyang21@bu.edu>
2024-12-11 15:36:31 -0500

    commit1

---
+++ src/bfs.rs
@@ -0,0 +1,60 @@
+use std::collections::VecDeque;
+
+//This function computes a BFS for each node in the graph and returns a vector of vectors
+//Which has the distances for each node to every other node in the graph
+//It takes a while to run for my selected data set as it has 37000 nodes
+//I used the lecture on BFS for this function and also researched online
+pub fn computeALLBFS(adj: &Vec<Vec<usize>>) -> Vec<Vec<Option<u32>>> {
+    let mut all_distances: Vec<Vec<Option<u32>>> = Vec::new();
+    for node in 0..adj.len() {
+        let mut distance: Vec<Option<u32>> = vec![None; adj.len()];
+        distance[node] = Some(0); // <= we know this distance
+        let mut queue: VecDeque<usize> = VecDeque::new();
+        queue.push_back(node);
+        while let Some(vertex) = queue.pop_front() {
+            for &current in &adj[vertex] {
+                if distance[current].is_none() {
+                    distance[current] = Some(distance[vertex].unwrap() + 1);
+                    queue.push_back(current);
+                }
+            }
+        }   
+        all_distances.push(distance);
+        println!("{}",node);  //I added this line so the user can know when the BFS's are almost done (when the number reaches 37000)
+    }
+    return all_distances
+}
+//This function prints ALL the BFS's for each node. IT takes a while to run 
+//I seperated the function computeALLBFS and printALLBFS because running them together took too long and gave my computer issues
+//I used the lecture on BFS for this function and also researched online
+pub fn printALLBFS(distances: &[Vec<Option<u32>>]) {
+    for (current_node, distances) in distances.iter().enumerate() {
+        print!("BFS for node {}: ", current_node);
+        for (v, dist) in distances.iter().enumerate() {
+            print!("{}:{} ", v, dist.unwrap());
+        }
+        println!();
+    }
+}
+
+//This function does a BFS to find distances for any node up to any node
+//Meaning if the input is start: 10 and lastnode = 100, then it will tell the shortest connections for node 10
+// From node 0 to node 100
+pub fn oneBFS(selected: usize, lastnode: usize, adj: &Vec<Vec<usize>>) {
+    let mut distance: Vec<Option<u32>> = vec![None; adj.len()];
+    distance[selected] = Some(0); //VS code Rust compilior told me to do the Some(0)
+    let mut queue: VecDeque<usize> = VecDeque::new();
+    queue.push_back(selected);
+    while let Some(node) = queue.pop_front() {
+        for &current in &adj[node] {
+            if distance[current].is_none() {
+                distance[current] = Some(distance[node].unwrap() + 1);
+                queue.push_back(current);
+            }
+        }
+    }
+    print!("Distances from BFS for node {} (from 0 up to node {}): ", selected, lastnode);
+    for (node, dist) in distance.iter().enumerate().take(lastnode + 1) { //The internet helped me with this line
+        print!("{}:{} ", node, dist.unwrap());
+    }
+}
---
+++ src/graph_analysis.rs
@@ -0,0 +1,59 @@
+pub fn average_distance(distances: &Vec<Vec<Option<u32>>>) -> f64 {
+    let mut total = 0;
+    let mut count: usize = 0;
+    for distance in distances.iter() {
+        for &current in distance.iter() {
+            if let Some(current) = current {
+                total += current as usize;
+                count += 1;
+            }
+        }
+    }
+    let average = total as f64 / count as f64;
+    return average;
+}
+//This function finds the nodes in the data set which have the highest degrees of freedom away from each other
+//This works by finding all of the distances between every node and iterating through them
+//It outputs a vector with all of node pairs with the furthest distance
+pub fn furthest(all_distances: &Vec<Vec<Option<u32>>>) -> Vec<(usize, usize, u32)> {
+    let mut maxd = 0;
+    let mut furthest = Vec::new();
+    for (nodeone, distances) in all_distances.iter().enumerate() {
+        for (nodetwo, &distance) in distances.iter().enumerate() {
+            if let Some(d) = distance {
+                if d > maxd {
+                    maxd = d;
+                    furthest.clear(); 
+                    furthest.push((nodeone, nodetwo, maxd));
+                } else if d == maxd {
+                    furthest.push((nodeone, nodetwo, maxd));
+                }
+            }
+        }
+    }
+    return furthest
+}
+
+//This function calculates the distribution for distances between nodes in the graph
+//The user inputs the amount of degrees they want to know the distribution for 
+//And the function returns the percentage of total connections occur with in x degrees of seperation
+pub fn degree_distribution(distances: &Vec<Vec<Option<u32>>>, degrees: u32) -> f64 {
+    let mut total: u32 = 0;
+    let mut target: u32 = 0;
+    for (nodeone, node) in distances.iter().enumerate() {
+        for (nodetwo, &d) in node.iter().enumerate() {
+            if nodeone != nodetwo { //This gets rid of nodes with distance 0 
+                if let Some(distance) = d { //The internet helped me with this line of code
+                    total += 1;
+                    if distance == degrees {
+                        target += 1;
+                    }   
+                }
+            }
+        }
+    }
+
+    let percent = (target as f64 / total as f64) * 100.0;
+    
+    return percent;
+}
